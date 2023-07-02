b99b4d275d5923bdb0cebad0abc1ddca1385e009
drchachere <drchachere@gmail.com>
2023-07-02 12:27:47 -0400

    updated model, now the Conf_Rk feature is turned into dummy features (rather than being scaled)

--- ncaa-tourn-qual-flask-app/app.py
+++ ncaa-tourn-qual-flask-app/app.py
@@ -25,18 +25,25 @@ def index():
             flash('No selected file')
             return redirect(request.url)
         file_name = "_"+file.filename
-        file.save(file_name)
+        if allowed_file(file.filename):
+            file.save(file_name)
+        # print(file_name)
         if file and allowed_file(file.filename):
             user_df = pd.read_csv(file_name)
             model = joblib.load("ncaa_tourn_qual_model_rfc.pkl")
             scaler = joblib.load("ncaa_tourn_qual_scaler.pkl")
+            user_df.columns = list(map(lambda x: x.strip(), user_df.columns))
             try:
-                df_scaled = scaler.transform(user_df.iloc[:, 0:24])
-                data_scaled = pd.DataFrame(df_scaled, columns=['G', 'W', 'L', 'W-L%', 'SRS', 'SOS', 'W.1', 'L.1', 'W.2', 'L.2', 'W.3', 'L.3', 'Tm.', 'Opp.', 'FTr', '3PAr', 'TS%', 'TRB%', 'AST%', 'BLK%', 'eFG%', 'TOV%', 'FT/FGA', 'Conf_Rk'])
+                df_scaled = scaler.transform(user_df.iloc[:, 0:23])
+                data_scaled = pd.DataFrame(df_scaled, columns=['G', 'W', 'L', 'W-L%', 'SRS', 'SOS', 'W.1', 'L.1', 'W.2', 'L.2', 'W.3', 'L.3', 'Tm.', 'Opp.', 'FTr', '3PAr', 'TS%', 'TRB%', 'AST%', 'BLK%', 'eFG%', 'TOV%', 'FT/FGA'])
+                data_ord = pd.get_dummies(user_df.iloc[:,23])
+                data_ord = pd.DataFrame(data_ord, columns=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16])
+                data_ord.columns = data_ord.columns.astype(str)
+                data_ord = data_ord.fillna(0)
                 data_dum = pd.get_dummies(user_df.iloc[:,24])
                 data_dum = pd.DataFrame(data_dum, columns=['WAC', 'MAC', 'SEC', 'SWAC', 'CAA', 'Southern', 'Pac-10', 'Sun Belt', 'Patriot', 'OVC', 'SWC', 'MEAC', 'Big Sky', 'Big East', 'NAC', 'MVC', 'Ivy', 'MW Coll', 'Big West', 'Big South', 'MAAC', 'TAAC', 'Metro', 'GMWC', 'ACC', 'Mid-Cont', 'Big 8', 'NEC', 'WCC', 'Big Ten', 'Southland', 'A-10', 'ECC', 'AWC', 'CUSA', 'Big 12','AEC', 'MWC', 'A-Sun', 'Horizon', 'Summit', 'GWC', 'Pac-12', 'AAC'])
                 data_dum = data_dum.fillna(0)
-                data_ml = pd.concat([data_scaled, data_dum], axis=1)
+                data_ml = pd.concat([data_scaled, data_ord, data_dum], axis=1)
                 data_ml = numpy.array(data_ml)
                 result = model.predict(data_ml)
             except:
